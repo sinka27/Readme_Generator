00ad1a0e05ec1b8a2e359cb7b676e23273ee9f6a
sinka27 <sinka27392@gmail.com>
2022-01-23 23:28:36 -0800

    Created a function to generate markdown for README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,7 +14,39 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+  ## Licensing:
+  [![license](https://img.shields.io/badge/license-${data.licensing}-blue)](https://shields.io)
+  
+  ## Table of Contents 
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Testing](#testing)
+  - [Additional Info](#additional-info)
+
+  ## Description:
+  ${data.description}
+
+  ## Installation:
+  ${data.installation}
+
+  ## Usage:
+  ${data.usage}
+
+  ## Contribution:
+  ${data.contribution}
+
+  ## Testing:
+  ${data.testing}
+
+  ## License:
+  ${data.licensing}
+
+  ## Additional Info:
+  - Github: [${data.github}](https://github.com/${data.github})
+  - Email: ${data.email} `;
+
 }
 
 module.exports = generateMarkdown;
