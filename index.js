3a80b8f75970a3aeb60064f11f165007defdde35
sinka27 <sinka27392@gmail.com>
2022-01-23 23:17:05 -0800

    1. Updated Questions Array.  2.Created function to write README file. 3. Created function to initialize app.

--- index.js
+++ index.js
@@ -1,51 +1,84 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
 const questions = [
+    //Project Title
     {
         type: 'input',
         name: 'title',
         message: 'What is the title of the project? (Required)',   
       },
+      //Project Description
       {
         type: 'input',
         name: 'description',
         message: 'Provide a description of the project (Required)', 
       },
-      {
+      // Installation Instruction
+    {
         type: 'input',
-        name: 'motivation',
-        message: 'Motivation behind this project (Required)', 
-      },
-    //   {
-    //     type: 'input',
-    //     name: 'problemsSolved',
-    //     message: 'What problems were solved by this project? (Required)', 
-    //   },
-      {
+        name: 'installation',
+        message: 'How do you install this project? (Required)',
+    },
+    // Usage Information
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How to use this project? (Required)',
+    },
+    // Contribution Guidlines
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'How should people contribute to this project? (Required)',
+    },
+     // Test Instructions 
+     {
+        type: 'input',
+        name: 'testing',
+        message: 'How do you test this project? (Required)',
+     },
+       // License Options
+       {
         type: 'checkbox',
-        name: 'tools&Technologies',
-        message: 'What Tools & Technologies were used for this project? (Required)', 
-        choices: ['Visual Studio Code','HTML', 'CSS', 'JavaScript', 'Git', 'GitHub', 'node.js', 'npm', ],
-      },
+        name: 'licensing',
+        message: 'Choose a license for your project (Required)',
+        choices: ['Apache', 'MIT', 'Mozilla-Public', 'GNU-General-Public', 'Common-Development-and Distribution', 'None'],
+    },
+
+      //GitHub user name
       {
         type: 'input',
-        name: 'gitHubRepo',
-        message: 'Enter your GitHub Repository URL: (Required)', 
+        name: 'gitHubUserName',
+        message: 'Enter your GitHub user name: (Required)', 
       },
+
+       // Email Address
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email?',
+    },
+    
 
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-  
+    fs.writeFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log('Information transferred to the README successfully!'))
 }
 
 // TODO: Create a function to initialize app
 function init() {
-
+inquirer.prompt(questions)
+.then(function userInput() {
+    console.log(userInput)
+        writeToFile("generatedREADME.md", generateMarkdown(userInput)); 
+})
 }
 
 // Function call to initialize app
