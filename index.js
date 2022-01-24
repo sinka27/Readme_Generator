6a9e7552cfa50df88b934c0035f268ad9310b535
sinka27 <sinka27392@gmail.com>
2022-01-23 23:28:02 -0800

    Added conditions to validate if user has entered information or not.

--- index.js
+++ index.js
@@ -9,37 +9,85 @@ const questions = [
     {
         type: 'input',
         name: 'title',
-        message: 'What is the title of the project? (Required)',   
+        message: 'What is the title of the project? (Required)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('You need to enter a title to continue!');
+                return false;
+            }   
+        }
       },
       //Project Description
       {
         type: 'input',
         name: 'description',
-        message: 'Provide a description of the project (Required)', 
+        message: 'Provide a description of the project (Required)',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('You need to enter description to continue!');
+                return false;
+            }   
+        }
       },
       // Installation Instruction
     {
         type: 'input',
         name: 'installation',
         message: 'How do you install this project? (Required)',
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log('You need to provide installation info to continue!');
+                return false;
+            }
+        }
     },
     // Usage Information
     {
         type: 'input',
         name: 'usage',
         message: 'How to use this project? (Required)',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('You need to provide information on how to use project!');
+                return false;
+            }
+        }
     },
     // Contribution Guidlines
     {
         type: 'input',
         name: 'contribution',
         message: 'How should people contribute to this project? (Required)',
+        validate: contributionInput => {
+            if (contributionInput) {
+                return true;
+            } else {
+                console.log('You need to provide information on how to contribute to the project!');
+                return false;
+            }
+        }
     },
      // Test Instructions 
      {
         type: 'input',
         name: 'testing',
         message: 'How do you test this project? (Required)',
+        validate: testingInput => {
+            if (testingInput) {
+                return true;
+            } else {
+                console.log('You need to describe how to test this project!');
+                return false;
+            }
+        }
      },
        // License Options
        {
@@ -47,6 +95,14 @@ const questions = [
         name: 'licensing',
         message: 'Choose a license for your project (Required)',
         choices: ['Apache', 'MIT', 'Mozilla-Public', 'GNU-General-Public', 'Common-Development-and Distribution', 'None'],
+        validate: licensingInput => {
+            if (licensingInput) {
+                return true;
+            } else {
+                console.log('You must pick a license for the project!');
+                return false;
+            }
+        }
     },
 
       //GitHub user name
@@ -54,6 +110,14 @@ const questions = [
         type: 'input',
         name: 'gitHubUserName',
         message: 'Enter your GitHub user name: (Required)', 
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username!');
+                return false;
+            }
+        }
       },
 
        // Email Address
@@ -61,6 +125,14 @@ const questions = [
         type: 'input',
         name: 'email',
         message: 'Enter your email?',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('Please enter your email!');
+                return false;
+            }
+        }
     },
     
 
